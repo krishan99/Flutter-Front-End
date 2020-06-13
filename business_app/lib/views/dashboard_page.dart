9bd6a26ebfb8505f86b3e82089474e154de86ae9
Rishuv Mehta <bmehta892@gmail.com>
2020-06-13 07:56:47 -0400

    now multiple items show up

--- business_app/lib/views/dashboard_page.dart
+++ business_app/lib/views/dashboard_page.dart
@@ -71,24 +71,34 @@ class _DashboardPageState extends State<DashboardPage> {
               ],
             ),
             DraggableScrollableSheet(
-                initialChildSize: 0.8,
-                minChildSize: 0.8,
-                maxChildSize: 0.9,
-                builder: (context, _controller) {
-                  return Container(
-                    color: Colors.blue,
-                    child: SingleChildScrollView(
-                        controller: _controller,
-                        child: Container(
-                          color: Colors.white,
-                          child: Column(children: <Widget>[
-                            Container(
-                                padding: EdgeInsets.all(30),
-                                child: SidableListCell())
-                          ]),
-                        )),
-                  );
-                })
+              initialChildSize: 0.8,
+              minChildSize: 0.8,
+              maxChildSize: 0.9,
+              builder: (context, _controller) {
+                return Container(
+                  color: MyStyles.of(context).colors.background1,
+                  child: SingleChildScrollView(
+                      controller: _controller,
+                      child: Container(
+                        color: Colors.white,
+                        child: Column(
+                          children: 
+                            <Widget>[
+                              SizedBox(height: 15)
+                            ] + 
+                            List.generate(2, (index) {
+                                return Container(
+                                  padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                                  child: SidableListCell()
+                                );
+                              }
+                            )
+                        ),
+                      )
+                  ),
+                );
+              }
+            )
           ],
         ),
       ),
@@ -98,6 +108,9 @@ class _DashboardPageState extends State<DashboardPage> {
 
 class SidableListCell extends StatelessWidget {
   static const double borderRadius = 20;
+  final bool selected;
+
+  const SidableListCell({Key key, this.selected = false}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -106,13 +119,13 @@ class SidableListCell extends StatelessWidget {
         child: Container(
           decoration: BoxDecoration(
             color: Colors.transparent,
+            border: (selected) ? Border.all(width: 1, color: MyStyles.of(context).colors.accent) : null,
             borderRadius: BorderRadius.circular(borderRadius),
             boxShadow: [
               BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                // spreadRad ius: 5,
+                color: Colors.black.withOpacity(0.10),
                 blurRadius: 10,
-                offset: Offset(0, 3), // changes position of shadow
+                offset: Offset(0, 3),
               ),
             ],
           ),
@@ -123,9 +136,25 @@ class SidableListCell extends StatelessWidget {
               confirmDismiss: (direction) {
                 return Future.value(false);
               },
-              background: Container(color: Colors.green),
+
+              background: Container(
+                padding: EdgeInsets.all(15),
+                color: MyStyles.of(context).colors.secondary,
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Delete",
+                  style: MyStyles.of(context).textThemes.buttonActionText2,
+                ),
+              ),
+
               secondaryBackground: Container(
-                color: Colors.orange,
+                padding: EdgeInsets.all(15),
+                color: MyStyles.of(context).colors.accent,
+                alignment: Alignment.centerRight,
+                child: Text(
+                  "Notify",
+                  style: MyStyles.of(context).textThemes.buttonActionText2,
+                ),
               ),
               child: Container(
                 color: MyStyles.of(context).colors.background1,
@@ -161,6 +190,7 @@ class SidableListCell extends StatelessWidget {
               ),
             ),
           ),
-        ));
+        )
+      );
   }
 }
